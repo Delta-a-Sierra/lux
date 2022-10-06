a1d42b0f40277e276e0b6ab1754cba8870c97b91
delta <delta@DESKTOP-D1JCKA9.localdomain>
2022-10-06 21:39:48 +0100

    refactored signup/signin

--- frontend/src/components/inputs/emailInput.js
+++ frontend/src/components/inputs/emailInput.js
@@ -7,7 +7,7 @@ const EmailInput = ({ name, value, onChange, errorMsg, onBlur, touched }) => {
         htmlFor="email"
         className={`${
           errorMsg ? "border border-red-500" : ""
-        } flex items-center space-x-3 bg-gray-200 px-4 py-3 rounded-full w-full text-sm md:text-base`}
+        } flex items-center space-x-3 bg-gray-200 px-4 py-3 rounded-full w-full text-sm md:text-base lg:text-xl xl:text-2xl`}
       >
         <MdEmail size="1.4em" color="#7F7F7F" />
         <input
@@ -23,7 +23,7 @@ const EmailInput = ({ name, value, onChange, errorMsg, onBlur, touched }) => {
       <p
         className={`${
           touched && errorMsg ? "inline" : "hidden"
-        } mt-1 ml-5 text-red-500 font-nunito tracking-wider text-xs`}
+        } mt-1 ml-5 text-red-500 font-nunito tracking-wider text-xs lg:text-base xl:text-lg`}
       >
         {errorMsg}
       </p>
--- frontend/src/components/inputs/passwordInput.js
+++ frontend/src/components/inputs/passwordInput.js
@@ -15,7 +15,7 @@ const PasswordInput = ({
         htmlFor="email"
         className={`${
           errorMsg ? "border border-red-500" : ""
-        } flex items-center space-x-3 bg-gray-200 px-4 py-3 rounded-full w-full text-sm md:text-base`}
+        } flex items-center space-x-3 bg-gray-200 px-4 py-3 rounded-full w-full text-sm md:text-base lg:text-lg xl:text-2xl`}
       >
         <MdPassword size="1.4em" color="#7F7F7F" />
         <input
@@ -31,7 +31,7 @@ const PasswordInput = ({
       <p
         className={`${
           touched && errorMsg ? "visible" : "hidden"
-        } mt-1 ml-5 text-red-500 font-nunito tracking-wider text-xs`}
+        } mt-1 ml-5 text-red-500 font-nunito tracking-wider text-xs lg:text-base xl:text-lg`}
       >
         {errorMsg ? errorMsg : "error"}
       </p>
